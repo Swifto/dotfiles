cd3ff34737b571ad41c9f8df5fd77010faf0d870
Ron Cohen <ronc@swifto.com>
2013-07-31 15:20:20 +0300

    add php and scss modes for emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -18,7 +18,7 @@
 (require 'compile)
 (ido-mode t)
 (menu-bar-mode -1)
-(normal-erase-is-backspace-mode 1)
+;;(normal-erase-is-backspace-mode 1)
 (put 'downcase-region 'disabled nil)
 (put 'upcase-region 'disabled nil)
 (setq column-number-mode t)
@@ -75,3 +75,33 @@
 (require 'jade-mode)    
 (add-to-list 'auto-mode-alist '("\\.styl$" . sws-mode))
 (add-to-list 'auto-mode-alist '("\\.jade$" . jade-mode))
+
+;; ---------------------------
+;; -- PHP Mode configuration --
+;; ---------------------------
+(load "php-mode")
+(add-to-list 'auto-mode-alist '("\\.module$" . php-mode))
+(add-to-list 'auto-mode-alist '("\\.inc$" . php-mode))
+(add-to-list 'auto-mode-alist '("\\.install$" . php-mode))
+(add-to-list 'auto-mode-alist '("\\.engine$" . php-mode))
+(add-to-list 'auto-mode-alist '("\\.profile$" . php-mode))
+
+;; Drupal recommended settings per http://sachachua.com/blog/2008/07/emacs-and-php-tutorial-php-mode/
+(defun wicked/php-mode-init ()
+   "Set some buffer-local variables."
+   (setq case-fold-search t) 
+   (setq indent-tabs-mode nil)
+   (setq fill-column 78)
+   (setq c-basic-offset 2)
+   (c-set-offset 'arglist-cont 0)
+   (c-set-offset 'arglist-intro '+)
+   (c-set-offset 'case-label 2)
+   (c-set-offset 'arglist-close 0))
+(add-hook 'php-mode-hook 'wicked/php-mode-init)
+
+;; ---------------------------
+;; -- SCSS Mode configuration --
+;; ---------------------------
+(load "scss-mode")
+(add-to-list 'auto-mode-alist '("\\.scss\\'" . scss-mode))
+(setq scss-compile-at-save nil)
